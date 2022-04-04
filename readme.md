0633d956fc4989378c6eb2abac0d04f906da9302
Alistair Smith <hi@alistair.sh>
2022-04-04 17:41:18 +0100

    fix: prettier project

--- readme.md
+++ readme.md
@@ -9,31 +9,31 @@
 #### Example
 
 ```ts
-import { Permer } from "permer";
+import {Permer} from 'permer';
 
-const permer = new Permer(["read", "write", "admin", "owner", "staff"]);
+const permer = new Permer(['read', 'write', 'admin', 'owner', 'staff']);
 
 const user = {
-  permissions: permer.calculate(["read", "write", "admin"]),
-  username: "alii",
+	permissions: permer.calculate(['read', 'write', 'admin']),
+	username: 'alii',
 };
 
 // Get individual permissions
-const isAdmin = permer.test(user.permissions, "admin");
-const isOwner = permer.test(user.permissions, "owner");
-const isStaff = permer.test(user.permissions, "staff");
-const canRead = permer.test(user.permissions, "read");
-const canWrite = permer.test(user.permissions, "write");
+const isAdmin = permer.test(user.permissions, 'admin');
+const isOwner = permer.test(user.permissions, 'owner');
+const isStaff = permer.test(user.permissions, 'staff');
+const canRead = permer.test(user.permissions, 'read');
+const canWrite = permer.test(user.permissions, 'write');
 
 console.log(`${user.username}'s permissions:`, {
-  isAdmin,
-  isOwner,
-  isStaff,
-  canRead,
-  canWrite,
+	isAdmin,
+	isOwner,
+	isStaff,
+	canRead,
+	canWrite,
 });
 
 // Get an array of all permissions
-const availablePermissions = permer.list(user.permissions).join(", ");
+const availablePermissions = permer.list(user.permissions).join(', ');
 console.log(`${user.username}'s permission list:`, availablePermissions);
 ```
--- tests/test.ts
+++ tests/test.ts
@@ -1,40 +1,40 @@
-import { test } from "uvu";
-import { Permer } from "../src";
-import * as assert from "uvu/assert";
+import {test} from 'uvu';
+import {Permer} from '../src';
+import * as assert from 'uvu/assert';
 
-const instance = new Permer(["create", "read", "update", "delete"]);
+const instance = new Permer(['create', 'read', 'update', 'delete']);
 
-const user1 = instance.calculate(["create", "read"]);
-const user2 = instance.calculate(["read", "update"]);
-const user3 = instance.calculate(["update", "delete"]);
+const user1 = instance.calculate(['create', 'read']);
+const user2 = instance.calculate(['read', 'update']);
+const user3 = instance.calculate(['update', 'delete']);
 
-test("Calculations", () => {
+test('Calculations', () => {
 	assert.is(user1, 3);
 	assert.is(user2, 6);
 	assert.is(user3, 12);
 });
 
-test("Additions", () => {
-	const added = instance.add(user1, ["delete"]);
-	const actual = instance.calculate(["create", "read", "delete"]);
+test('Additions', () => {
+	const added = instance.add(user1, ['delete']);
+	const actual = instance.calculate(['create', 'read', 'delete']);
 	assert.is(added, actual);
 });
 
-test("Subtractions", () => {
-	const subtracted = instance.subtract(user1, ["read"]);
-	const actual = instance.calculate(["create"]);
+test('Subtractions', () => {
+	const subtracted = instance.subtract(user1, ['read']);
+	const actual = instance.calculate(['create']);
 	assert.is(subtracted, actual);
 });
 
-test("Keys are correct", () => {
+test('Keys are correct', () => {
 	const keys = instance.keys();
 	assert.is(keys.length, 4);
-	assert.equal(keys, ["create", "read", "update", "delete"]);
+	assert.equal(keys, ['create', 'read', 'update', 'delete']);
 });
 
-test("Tests", () => {
-	const user1CanRead = instance.test(user1, "read");
-	const user1CanDelete = instance.test(user1, "delete");
+test('Tests', () => {
+	const user1CanRead = instance.test(user1, 'read');
+	const user1CanDelete = instance.test(user1, 'delete');
 
 	assert.is(user1CanRead, true);
 	assert.is(user1CanDelete, false);
